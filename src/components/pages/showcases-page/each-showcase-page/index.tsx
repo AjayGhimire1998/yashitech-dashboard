d010d9da53608e1fdb2716af9acdbcb1cac7f5d0
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-08-17 15:36:12 +1000

    added depen array in callback

--- src/components/pages/showcases-page/each-showcase-page/index.tsx
+++ src/components/pages/showcases-page/each-showcase-page/index.tsx
@@ -39,7 +39,7 @@ const EachShowCase: React.FunctionComponent = () => {
       console.log(error);
       console.log(error.response.data.error);
     }
-  }, []);
+  }, [id]);
 
   React.useEffect(() => {
     getShowcaseData();
