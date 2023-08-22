11b748f9d7c86c300a6a7a4028d022fed38c8b7e
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-08-22 22:48:10 +1000

    sometin wrong in prod

--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -107,7 +107,7 @@ export const FormAttribute = styled(Form)`
   flex-direction: column;
   align-items: center;
   justify-content: flex-start;
-  gap: 20px;
+  margin-top: 20px;
   /* @media (max-width: 600px) {
     width: 100%;
   } */
