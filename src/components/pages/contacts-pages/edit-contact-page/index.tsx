e65f366fd2556b8304744efaf4817fb3b6855f6c
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-09-05 18:16:19 +1000

    how to handle null err

--- src/components/pages/contacts-pages/edit-contact-page/index.tsx
+++ src/components/pages/contacts-pages/edit-contact-page/index.tsx
@@ -1,9 +1,15 @@
 import * as React from "react";
-import { Container, LoadingSpinner, Message } from "../../../../styles/global";
+import {
+  Container,
+  FormAttribute,
+  LoadingSpinner,
+  Message,
+} from "../../../../styles/global";
 import StaticContent from "../../../helper/pages-helpers/global-pages-helpers/StaticContent";
 import { useParams } from "react-router-dom";
 import { ContactParam } from "../view-contact-page";
 import { showContactData } from "../../../../services/other-services/contact-services";
+import { ShowcaseWrapper } from "../../showcases-pages/view-each-showcase-page/styles";
 
 interface EditContactPayload {
   name: string | undefined;
@@ -14,7 +20,18 @@ interface EditContactPayload {
   request_count: number | undefined;
 }
 
+type LimitedAttributes = {
+  name: string | undefined;
+  email: string | undefined;
+  budget: string | undefined;
+}
+
 const EditContact: React.FunctionComponent = () => {
+  const attributes: Array<string> = [
+    "name",
+    "email",
+    "budget",
+  ];
   const { id } = useParams<ContactParam>();
   const [message, setMessage] = React.useState<string>("");
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
@@ -57,6 +74,11 @@ const EditContact: React.FunctionComponent = () => {
   React.useEffect(() => {
     getContactData(id);
   }, [getContactData, id]);
+
+  //inputchange
+  const handleTextAreaChange = (e: React.ChangeEvent<HTMLTextAreaElement>): void => {
+    console.log(e.target.value);
+  };
   return (
     <Container>
       <StaticContent history="contacts" />
@@ -81,6 +103,19 @@ const EditContact: React.FunctionComponent = () => {
         <LoadingSpinner color="#440a70" height="50" width="50" />
       ) : contactPayload ? (
         <>
+          <ShowcaseWrapper>
+            {attributes.map((attr: string, index: number) => {
+              return (
+                <FormAttribute
+                  onChange={(e) => handleTextAreaChange(e)}
+                  key={index}
+                  attribute={attr}
+                  value={contactPayload[attr as keyof LimitedAttributes] }
+                />
+              );
+            })}
+            <br />
+          </ShowcaseWrapper>
         </>
       ) : (
         <p>Internal Server Error. Try Reloading.</p>
